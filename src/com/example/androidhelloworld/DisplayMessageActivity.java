3794931c1ace309b10f09ad20f6aedfb18bd5d08
Vikram Somu <vikster93@gmail.com>
2014-02-12 02:51:37 -0500

    Updated Action Bar

--- src/com/example/androidhelloworld/DisplayMessageActivity.java
+++ src/com/example/androidhelloworld/DisplayMessageActivity.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.support.v4.app.NavUtils;
 
 public class DisplayMessageActivity extends Activity {
 
@@ -16,41 +15,35 @@ public class DisplayMessageActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
-			// Show the Up button in the action bar.
-			setupActionBar();			
-		}
-		
-		String message = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);
-		TextView textView = new TextView(this);
+		setupActionBar();
+		setContentView(R.layout.activity_display_message);
+		String message = getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE);		
+		TextView textView = (TextView) findViewById(R.id.display_message_textview);
 		textView.setTextSize(40);
 		textView.setText(message);
-		
-		setContentView(textView);
-
 	}
 
 	/**
 	 * Set up the {@link android.app.ActionBar}.
 	 */
 	private void setupActionBar() {
-
-		getActionBar().setDisplayHomeAsUpEnabled(true);
-
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB){
+			// Show the Up button in the action bar.
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.display_message, menu);
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
-		case android.R.id.home:
+		case R.id.action_search:
 			// This ID represents the Home or Up button. In the case of this
 			// activity, the Up button is shown. Use NavUtils to allow users
 			// to navigate up one level in the application structure. For
@@ -58,10 +51,22 @@ public class DisplayMessageActivity extends Activity {
 			//
 			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
 			//
-			NavUtils.navigateUpFromSameTask(this);
+			openSearch();
 			return true;
-		}
-		return super.onOptionsItemSelected(item);
+		case R.id.action_settings:
+			openSettings();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);			
+		}	
+	}
+	
+	public void openSearch(){
+		
+	}
+	
+	public void openSettings(){
+		
 	}
 
 }
